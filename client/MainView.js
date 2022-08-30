8c752e419c31853b8c21e6bcb804b90ebc3eaf75
Josh Medlock <jmedsy@gmail.com>
2022-08-30 10:50:03 -0500

    First commit

---
+++ client/MainView.js
@@ -0,0 +1,6 @@
+import m from 'mithril';
+import Greeting from '../src/Greeting';
+
+export default {
+  view: vnode => m(Greeting)
+}
---
+++ src/Greeting.js
@@ -0,0 +1,5 @@
+import m from 'mithril';
+
+export default {
+    view: vnode => m('span.is-size-1', 'Hello, world!')
+}
